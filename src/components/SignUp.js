9c0f55b59064033d096748f54dbf14b39d1f69c1
Kevin <28754130+KOWDER@users.noreply.github.com>
2018-05-03 19:44:10 +0200

    add skeleton of SignUpForm

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,11 +1,100 @@
-import React from 'react';
+import React, { Component } from 'react';
 
+
+// Sign Up Page
 const SignUp = () => {
   return (
     <div>
-      Sign Up Page
+      <h1>Sign Up Page</h1>
+      <SignUpForm />
     </div>
   )
+};
+
+// Initial State
+const INITIAL_STATE = {
+  username: "",
+  email: "",
+  passwordOne: "",
+  passwordTwo: "",
+  error: null
+};
+
+// Sign Up Form
+class SignUpForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { ...INITIAL_STATE }
+  }
+
+  handleInput = (propertyName, value) => {
+    this.setState({
+      [propertyName]: value
+    })
+  }  
+
+  render() {
+    const { username, email, passwordOne, passwordTwo, error } = this.state;
+
+    const isInvalid =
+      passwordOne !== passwordTwo ||
+      passwordOne === "" ||
+      email === "" ||
+      username === "";
+
+    return (
+      <form>
+        <label for="username">Name: </label>
+        <input 
+          name="username"
+          value={username}
+          type="text"
+          onChange={event => this.handleInput('username', event.target.value)}
+          placeholder="your name..."
+        />
+        <br />
+        <label for="email">Email: </label>
+        <input 
+          name="email"
+          value={email}
+          type="email"
+          onChange={event => this.handleInput('email', event.target.value)}
+          placeholder="your email..."
+        />
+        <br />
+        <label for="passwordOne">Password: </label>
+        <input 
+          name="passwordOne"
+          value={passwordOne}
+          type="password"
+          onChange={event => this.handleInput('passwordOne', event.target.value)}
+          placeholder="enter password..."
+        />
+        <br />
+        <label for="passwordTwo">Confirm Password: </label>
+        <input 
+          name="passwordTwo"
+          value={passwordTwo}
+          type="password"
+          onChange={event => this.handleInput('passwordTwo', event.target.value)}
+          placeholder="confirm password..."
+        />
+        <br />
+        <button disabled={isInvalid}>Sign Up</button>
+        <br />
+        { error && <p>{error.message}</p> }
+      </form>
+    )
+  }
+
+
 }
+
+
+
+
+
+
 
 export default SignUp;
