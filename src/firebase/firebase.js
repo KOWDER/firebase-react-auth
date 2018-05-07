3bd66572c0c5e57871054473aa15f19b92739a77
Kevin <28754130+KOWDER@users.noreply.github.com>
2018-05-07 14:23:42 +0200

    protected apikey

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from "firebase";
 
 const config = {
-  apiKey: "AIzaSyB2ZAIZ3xv09aNAjh2IdLgMOuArkux2FU0",
-  authDomain: "auth-react-275e1.firebaseapp.com",
-  databaseURL: "https://auth-react-275e1.firebaseio.com",
-  projectId: "auth-react-275e1",
+  apiKey: "YOUR_APIKEY",
+  authDomain: "...",
+  databaseURL: "...",
+  projectId: "...",
   storageBucket: "",
-  messagingSenderId: "218908481822"
+  messagingSenderId: "..."
 };
 
 firebase.initializeApp(config);
